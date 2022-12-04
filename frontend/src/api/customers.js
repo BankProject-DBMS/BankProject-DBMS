6289352b8d4d6ef4c4d7b52cd965a496ba9e0c04
Radith <jjeli1234@gmail.com>
2022-12-04 12:22:09 +0530

    Changed to callback

--- frontend/src/api/customers.js
+++ frontend/src/api/customers.js
@@ -1,12 +1,19 @@
 import axios from 'axios';
 import { HOST } from './config';
 
-export async function getCustomers() {
+export function getCustomers() {
+  /*
   try {
     const res = await axios.post(`${HOST}/customers`);
     return res.data;
   } catch (err) {
     console.log(err);
     return await Promise.reject(err);
-  }
+  }*/
+  return axios
+    .get(`${HOST}/branches`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => Promise.reject('Failed to get customers list!'));
 }
