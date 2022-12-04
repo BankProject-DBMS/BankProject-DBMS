cabaa0f528c63f09e6699dda7bd13f4fa6721a1f
inuka-00 <77762018+inuka-00@users.noreply.github.com>
2022-12-04 15:25:44 +0530

    multiple pages
    multiple pages connected with links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,25 @@ import './App.css';
 // import AuthenticationForm from './Forms/AuthenticationForm';
 import CustomerReg from './Forms/CustomerReg';
 import CustomerList from './Forms/CustomerList';
+import EmployeeHome from './pages/EmployeeHome';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <CustomerReg className="centered" />
-      <CustomerList className="customer" />
+      {/* <CustomerReg className="centered" /> */}
+      {/* <CustomerList className="customer" /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' >
+            <Route exact path="/" element={<EmployeeHome />} />
+            <Route path='/customer-register' element={<CustomerReg />} />
+            <Route path='/customer-list' element={<CustomerList />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+
     </div >
   );
 }
---
+++ frontend/src/pages/EmployeeHome.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function EmployeeHome() {
+    return (
+        <div>
+            <div>
+                <a href="/customer-register" >Register New Customer</a>
+            </div>
+            <div>
+                <a href="/customer-list" >Customer List</a>
+            </div>
+        </div>
+    )
+}
