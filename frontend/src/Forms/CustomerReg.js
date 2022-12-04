6583733071be25784ba43a9d6a6c756ccad4e626
Radith <jjeli1234@gmail.com>
2022-12-04 18:01:53 +0530

    Basic Customer Reg Page

--- frontend/src/Forms/CustomerReg.js
+++ frontend/src/Forms/CustomerReg.js
@@ -1,48 +1,75 @@
-import React, { useState } from "react";
-import { addCustomer } from "../api/customers";
+import React, { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Button } from 'antd';
+import { addCustomer } from '../api/customers';
+import * as Yup from 'yup';
 
 export default function CustomerReg() {
-    const [name, setName] = useState('')
-    const [dateofbirth, setDate] = useState('')
-    const [address, setAddress] = useState('')
-    const [phone, setPhone] = useState('')
-    const [occupation, setOccupation] = useState('')
+  const customerRegSchema = Yup.object().shape({
+    name: Yup.string().required(),
+    dob: Yup.date().required(),
+    address: Yup.string().required(),
+    phone: Yup.string()
+      .matches(/(^[0-9]+$|^\+[0-9]+$)/, 'Invalid Phone Number')
+      .min(10, 'Phone number must be at least 10 digits')
+      .max(12, 'Phone number must be at most 12 digits')
+      .required(),
+    occupation: Yup.string().required(),
+  });
+  const handleSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
 
+      setSubmitting(false);
+    }, 400);
+  };
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          name: '',
+          dob: '',
+          address: '',
+          phone: '',
+          occupation: '',
+        }}
+        validationSchema={customerRegSchema}
+        onSubmit={handleSubmit}
+      >
+        {(props) => (
+          <Form className='customer--reg--form'>
+            <span>
+              <Field type='text' name='name' placeholder='Full Name' />
+              <ErrorMessage name='name' component='span' />
+            </span>
+            <span>
+              <Field type='date' name='dob' placeholder='Date of Birth' />
+              <ErrorMessage name='dob' component='span' />
+            </span>
+            <span>
+              <Field type='text' name='address' placeholder='Address' />
+              <ErrorMessage name='address' component='span' />
+            </span>
+            <span>
+              <Field type='text' name='phone' placeholder='Phone' />
+              <ErrorMessage name='phone' component='span' />
+            </span>
+            <span>
+              <Field type='text' name='occupation' placeholder='Occupation' />
+              <ErrorMessage name='occupation' component='span' />
+            </span>
 
-    function handleSubmit(event) {
-        //console.log({ name, date, address, phone, occupation });
-        console.log("submitted");
-
-        const customer = { name, dateofbirth, address, phone, occupation };
-        addCustomer({ customer });
-        event.preventDefault();
-
-    }
-
-    // TODO Consider using https://formik.org
-    return (
-        <div className="form">
-            <label>
-                <p>Name</p>
-                <input name="name" value={name} placeholder="Full Name" onChange={(e) => setName(e.target.value)} />
-            </label>
-            <label>
-                <p>Date of Birth</p>
-                <input type='date' value={dateofbirth} name="dob" onChange={(e) => setDate(e.target.value)} />
-            </label>
-            <label>
-                <p>Address</p>
-                <input name="address" value={address} placeholder="Address" onChange={(e) => setAddress(e.target.value)} />
-            </label>
-            <label>
-                <p>Phone</p>
-                <input value={phone} name="phone" placeholder="Phone" onChange={(e) => setPhone(e.target.value)} />
-            </label>
-            <label>
-                <p>Occupation</p>
-                <input value={occupation} name="occupation" placeholder="Occupation" onChange={(e) => setOccupation(e.target.value)} />
-            </label>
-            <br />
-            <button onClick={handleSubmit}>Submit</button>
-        </div>)
-}    
+            <Button
+              className='customer--reg--form--submit'
+              type='primary'
+              onClick={props.handleSubmit}
+              disabled={props.isSubmitting}
+            >
+              Submit
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
