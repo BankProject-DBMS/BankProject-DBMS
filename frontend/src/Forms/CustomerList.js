704ba725be29086b61da522e6e9369b64a281ae4
Radith <jjeli1234@gmail.com>
2022-12-12 11:11:13 +0530

    sorting by dateOfBirth

--- frontend/src/Forms/CustomerList.js
+++ frontend/src/Forms/CustomerList.js
@@ -1,58 +1,56 @@
-import React from "react";
+import React from 'react';
 
-
-import { getCustomers } from "../api/customers";
-import { Table } from "antd";
+import { getCustomers } from '../api/customers';
+import { Table } from 'antd';
 
 export default function CustomerList() {
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'Name',
+      key: 'Name',
+    },
+    {
+      title: 'DoB',
+      dataIndex: 'dateofbirth',
+      key: 'dateofbirth',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => {
+        return Date.parse(a.dateofbirth) - Date.parse(b.dateofbirth);
+      },
+    },
+    {
+      title: 'Address',
+      dataIndex: 'Address',
+      key: 'Address',
+    },
+  ];
 
+  const [Customer, setCustomer] = React.useState();
+  //const [loading, setLoading] = useState(true);
 
+  React.useEffect(() => loadCustomerList(), []);
 
-
-
-    const columns = [
-        {
-            title: 'Name',
-            dataIndex: 'Name',
-            key: 'Name',
-        },
-        {
-            title: 'DoB',
-            dataIndex: 'dateofbirth',
-            key: 'dateofbirth',
-        },
-        {
-            title: 'Address',
-            dataIndex: 'Address',
-            key: 'Address',
-        },
-    ];
-
-    const [Customer, setCustomer] = React.useState();
-    //const [loading, setLoading] = useState(true);
-
-    React.useEffect(() => loadCustomerList(), []);
-
-    /*const onDelete = (id) => {
+  /*const onDelete = (id) => {
       deleteEmployee(id);
       loadEmployeeList();
     };*/
 
-    function loadCustomerList() {
-        getCustomers()
-            .then((data) => {
-                setCustomer(data);
-            })
-            .catch((err) => alert(err));
-    }
+  function loadCustomerList() {
+    getCustomers()
+      .then((data) => {
+        setCustomer(data);
+      })
+      .catch((err) => alert(err));
+  }
 
-    //loadCustomerList();
-    //console.log(Customer);
-    return (<div>
+  //loadCustomerList();
+  //console.log(Customer);
+  return (
+    <div>
+      <h1>Customer List</h1>
 
-        <h1>Customer List</h1>
-
-        {<Table dataSource={Customer} columns={columns} />}
-
-    </div>);
+      {<Table dataSource={Customer} columns={columns} />}
+    </div>
+  );
 }
