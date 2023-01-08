187324c0fe2df3d5ba83dcc816a9177bb76b437a
inuka-00 <77762018+inuka-00@users.noreply.github.com>
2023-01-08 23:31:44 +0530

    online and physical loans fixed in customer home

--- backend/app/controllers/onlineloanController.js
+++ backend/app/controllers/onlineloanController.js
@@ -2,19 +2,35 @@ const OnlineLoanModel = require('../models/onlineloan.model');
 
 //
 exports.getCustomerOnlineLoans = (req, res) => {
-  const customerID = req.user.CustomerID;
-  OnlineLoanModel.getAll(customerID, (err, data) => {
-    if (err.kind === 'not_found') {
-      res.status(404).send({
-        message: `No online loans found for customer ${customerID}.`,
-      });
-    } else if (err.kind != 'success') {
-      res.status(500).send({
-        message:
-          err.message || 'Some error occurred while retrieving accounts.',
-      });
-    } else res.send(data);
-  });
+  console.log('In oloan.contr');
+  if (req.user.CustomerID) {
+    const customerID = req.user.CustomerID;
+    OnlineLoanModel.getAll(customerID, (err, data) => {
+      if (err.kind === 'not_found') {
+        res.status(404).send({
+          message: `No online loans found for customer ${customerID}.`,
+        });
+      } else if (err.kind != 'success') {
+        res.status(500).send({
+          message:
+            err.message || 'Some error occurred while retrieving accounts.',
+        });
+      } else res.send(data);
+    });
+  } else {
+    OnlineLoanModel.getAll((err, data) => {
+      if (err.kind === 'not_found') {
+        res.status(404).send({
+          message: `No online loans found for customer ${customerID}.`,
+        });
+      } else if (err.kind != 'success') {
+        res.status(500).send({
+          message:
+            err.message || 'Some error occurred while retrieving accounts.',
+        });
+      } else res.send(data);
+    });
+  }
 };
 
 exports.getAccountInstallments = (req, res) => {
--- backend/app/routes/deposit.routes.js
+++ backend/app/routes/deposit.routes.js
@@ -1,13 +1,13 @@
 module.exports = (app) => {
-    const deposites = require('../controllers/depositController');
-  
-    const router = require('express').Router();
-  
-    router.get('/', deposites.findAll);
-  
-    router.post('/add', deposites.create);
+  const deposites = require('../controllers/depositController');
 
-    router.get('/findByID',deposites.findByAccountID);
-  
-    app.use('/deposites', router);
-  };
+  const router = require('express').Router();
+
+  router.post('/', deposites.findAll);
+
+  router.post('/add', deposites.create);
+
+  router.get('/findByID', deposites.findByAccountID);
+
+  app.use('/deposites', router);
+};
