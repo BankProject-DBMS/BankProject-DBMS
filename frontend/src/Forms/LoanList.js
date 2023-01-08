0c810d936ff3a63e1485dc31b6cd0e3e06f290a3
Udara Wijesinghe <udarawijesinghe20@gmail.com>
2023-01-08 20:43:02 +0530

    UI

--- frontend/src/Forms/LoanList.js
+++ frontend/src/Forms/LoanList.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { getLoans } from '../api/loans';
 import { Table } from 'antd';
+import { Navigate, useNavigate, Outlet } from 'react-router-dom';
+import Logo from '../pages/Images/Logo2.png';
+import '../pages/PageStyling/Navbar.css';
 
 export default function LoanList() {
   const columns = [
@@ -59,14 +62,23 @@ export default function LoanList() {
       })
       .catch((err) => console.log(err));
   }
+  const navigate = useNavigate();
 
   //loadLoanList();
   //console.log(loans);
   return (
     <div>
-      <h1>Loan List</h1>
-
-      {<Table dataSource={loans} columns={columns} />}
+      <div className='navbar'>
+        <img
+          className='aruci--logo'
+          src={Logo}
+          onClick={() => navigate('/employeePortal/')}
+        />
+        <h1 className='topic'>Loan List</h1>
+      </div>
+      <div className='table'>
+        {<Table dataSource={loans} columns={columns} />}
+      </div>
     </div>
   );
 }
