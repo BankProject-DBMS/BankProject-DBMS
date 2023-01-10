e366568dddb2edb5c8e5ae0cdcb5b889c0b0a395
Radith Samarakoon <jjeli1234@gmail.com>
2023-01-10 14:11:54 +0530

    wip

--- frontend/src/pages/CustomerPortalPages/PhysicalLoanView.js
+++ frontend/src/pages/CustomerPortalPages/PhysicalLoanView.js
@@ -4,19 +4,24 @@ import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import Logo from '../Images/Logo2.png';
 
-import { getCustomerPhysicalLoans, getPhysicalLoanInstallment } from '../../api/physloans';
+import {
+  getCustomerPhysicalLoans,
+  getPhysicalLoanInstallment,
+} from '../../api/physloans';
 
 export default function PhysicalLoanView() {
   const [loan, setLoan] = useState();
   const [physicalLoanInstallment, setPhysicalLaonInstallment] = useState();
 
-  const { loanID } = useParams();
+  const { physicalLoanID } = useParams();
 
   useEffect(() => {
-    console.log(loanID);
-    getCustomerPhysicalLoans(loanID).then((data) => setLoan(data));
-    getPhysicalLoanInstallment(loanID).then((data) => setPhysicalLaonInstallment(data));
-  }, [loanID]);
+    console.log(physicalLoanID);
+    getCustomerPhysicalLoans(physicalLoanID).then((data) => setLoan(data));
+    getPhysicalLoanInstallment(physicalLoanID).then((data) =>
+      setPhysicalLaonInstallment(data)
+    );
+  }, [physicalLoanID]);
 
   const columns = [
     {
@@ -45,7 +50,11 @@ export default function PhysicalLoanView() {
   return (
     <div>
       <div className='navbar'>
-        <img className='aruci--logo' src={Logo} onClick={() => navigate('/customerPortal/')} />
+        <img
+          className='aruci--logo'
+          src={Logo}
+          onClick={() => navigate('/customerPortal/')}
+        />
         <h1 className='topic'>Physical Loan Details</h1>
       </div>
       <div style={{ padding: '1%', margin: '2%', border: 'solid' }}>
@@ -92,7 +101,6 @@ export default function PhysicalLoanView() {
           <b>Interest Rate: </b>
           {loan?.InterestRate}
         </p>
-        
       </div>
 
       <div style={{ margin: '2% 2%' }}>
